e03ba3a69639c69b8bbdfd16bcf5be2fec9452e9
Samuel Vila <samuel.vila@hpe.com>
2024-05-21 14:30:40 +0200

    Create spaceship table in database
    In this commit, a new table named 'spaceship' is created in the database. This spaceship table contains fields such as id, name, franchise, and max_speed. This prepares the database to store and handle data related to spaceships.

---
+++ src/main/resources/db/migration/V1__Create_spaceship_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE spaceship (
+                           id IDENTITY PRIMARY KEY,
+                           name VARCHAR(255),
+                           franchise VARCHAR(255),
+                           max_speed DOUBLE
+);
