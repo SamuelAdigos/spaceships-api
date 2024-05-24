6a94c045d2f13d566eda1953a4d4788933991df1
Samuel Vila <samuel.vila@hpe.com>
2024-05-24 12:09:16 +0200

    Add initial spaceship data to MongoDB
    This commits adds a JavaScript file 'init-mongo.js', which creates a new sibling database 'spaceships', and a 'spaceship' collection in MongoDB. It then adds 10 entries to the database with fields such as name, franchise, and max speed of popular fiction spaceships. The file also includes checks to prevent re-initialization of the database.

---
+++ init-mongo.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const path = '/data/db/.initialized';
+
+if (fs.existsSync(path)) {
+  print("The database has already been initialized. Exiting...");
+  quit();
+}
+
+function generateUUID() {
+  let d = new Date().getTime();
+  if (typeof performance !== 'undefined' && typeof performance.now
+      === 'function') {
+    d += performance.now();
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
+db = db.getSiblingDB('spaceships');
+
+db.createCollection('spaceship');
+db.spaceship.insertMany([
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Enterprise",
+    franchise: "Star Trek",
+    maxSpeed: 8.0
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Millennium Falcon",
+    franchise: "Star Wars",
+    maxSpeed: 3.0
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Serenity",
+    franchise: "Firefly",
+    maxSpeed: 2.5
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Galactica",
+    franchise: "Battlestar Galactica",
+    maxSpeed: 2.0
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Discovery One",
+    franchise: "2001: A Space Odyssey",
+    maxSpeed: 1.5
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Rocinante",
+    franchise: "The Expanse",
+    maxSpeed: 6.5
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Andromeda Ascendant",
+    franchise: "Andromeda",
+    maxSpeed: 9.0
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Heart of Gold",
+    franchise: "Hitchhiker's Guide to the Galaxy",
+    maxSpeed: 10.0
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Eagle 5",
+    franchise: "Spaceballs",
+    maxSpeed: 1.0
+  },
+  {
+    _id: generateUUID(),
+    _class: "com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.SpaceshipData",
+    name: "Nostromo",
+    franchise: "Alien",
+    maxSpeed: 1.2
+  }
+]);
+
+fs.writeFileSync(path, 'Initialization completed');
+print("Initial data has been inserted. Exiting...");
