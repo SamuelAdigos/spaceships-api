35df4b1b218cbf5a722d87a96f5041ff548d5ca6
Samuel Vila <samuel.vila@hpe.com>
2024-05-24 12:21:08 +0200

    Refactor spaceship update tests and streamline command
    Reworked the UpdateSpaceshipCommandHandlerShould tests to add clarity by creating a nested class for conditions when a spaceship exists. This update simplifies the testing of command handling. Simultaneously, UpdateSpaceshipCommandMother has also been upgraded for a smoother command creation.

--- src/test/java/com/samuel/spaceships/api/Application/Update/UpdateSpaceshipCommandMother.java
+++ src/test/java/com/samuel/spaceships/api/Application/Update/UpdateSpaceshipCommandMother.java
@@ -1,42 +1,20 @@
 package com.samuel.spaceships.api.Application.Update;
 
-public class UpdateSpaceshipCommandMother {
-  public static UpdateSpaceshipCommand withDefaults() {
-    return new UpdateSpaceshipCommand(1L, "Default Name", "Default Franchise", 2.0);
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipFranchise;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipFranchiseMother;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipId;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipIdMother;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipName;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipNameMother;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipSpeed;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipSpeedMother;
+
+public final class UpdateSpaceshipCommandMother {
+  public static UpdateSpaceshipCommand create(SpaceshipId id, SpaceshipName name, SpaceshipFranchise franchise, SpaceshipSpeed maxSpeed) {
+    return new UpdateSpaceshipCommand(id.value(), name.value(),franchise.value(), maxSpeed.value());
   }
 
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-    private Long id = 1L;
-    private String name = "Default Name";
-    private String franchise = "Default Franchise";
-    private Double maxSpeed = 2.0;
-
-    public Builder withId(Long id) {
-      this.id = id;
-      return this;
-    }
-
-    public Builder withName(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public Builder withFranchise(String franchise) {
-      this.franchise = franchise;
-      return this;
-    }
-
-    public Builder withMaxSpeed(Double maxSpeed) {
-      this.maxSpeed = maxSpeed;
-      return this;
-    }
-
-    public UpdateSpaceshipCommand build() {
-      return new UpdateSpaceshipCommand(id, name, franchise, maxSpeed);
-    }
+  public static UpdateSpaceshipCommand random() {
+    return create(SpaceshipIdMother.random(), SpaceshipNameMother.random(), SpaceshipFranchiseMother.random(), SpaceshipSpeedMother.random());
   }
 }
