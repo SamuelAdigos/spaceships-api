e406a6cf8d0dfc0fc3681f414bf81ec44a2df816
Samuel Vila <samuel.vila@hpe.com>
2024-05-24 12:16:58 +0200

    Refactor update operation with domain classes
    Updated the update operation in Spaceship application to utilize the domain classes for id, name, franchise, and maxSpeed for improved consistency, readability, and code maintenance. Changes were applied across the relevant functions and parameters in the Update-related classes.

--- src/main/java/com/samuel/spaceships/api/Application/Update/SpaceshipUpdater.java
+++ src/main/java/com/samuel/spaceships/api/Application/Update/SpaceshipUpdater.java
@@ -3,7 +3,10 @@ package com.samuel.spaceships.api.Application.Update;
 import com.samuel.spaceships.api.Domain.Spaceship.Errors.SpaceshipNotExist;
 import com.samuel.spaceships.api.Domain.Spaceship.Spaceship;
 import com.samuel.spaceships.api.Domain.Spaceship.Repository.SpaceshipRepository;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipFranchise;
 import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipId;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipName;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipSpeed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
@@ -15,10 +18,9 @@ public class SpaceshipUpdater {
   private final SpaceshipRepository repository;
 
   @CacheEvict(value = "spaceshipById", key = "#id")
-  public void update(Long id, String name, String franchise, double maxSpeed) {
-    var spaceshipId = new SpaceshipId(id);
-    if (!repository.existsById(spaceshipId)) {
-      throw new SpaceshipNotExist(spaceshipId);
+  public void update(SpaceshipId id, SpaceshipName name, SpaceshipFranchise franchise, SpaceshipSpeed maxSpeed) {
+    if (!repository.existsById(id)) {
+      throw new SpaceshipNotExist(id);
     }
 
     var spaceship = new Spaceship(id, name, franchise, maxSpeed);
