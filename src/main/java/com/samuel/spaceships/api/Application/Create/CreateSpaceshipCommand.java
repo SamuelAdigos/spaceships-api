96d8a4cec899233d165d05a7129c87d49ddf7385
Samuel Vila <samuel.vila@hpe.com>
2024-05-24 12:16:18 +0200

    Refactor spaceship creation modules
    The spaceship creation modules `CreateSpaceshipCommand`, `CreateSpaceshipCommandHandler`, and `SpaceshipCreator` have been refactored. Imports have been changed to their updated versions, and the way a spaceship is created has been adjusted to include logging and specific spaceship attributes. A mechanism to generate unique spaceship IDs has also been introduced.

--- src/main/java/com/samuel/spaceships/api/Application/Create/CreateSpaceshipCommand.java
+++ src/main/java/com/samuel/spaceships/api/Application/Create/CreateSpaceshipCommand.java
@@ -1,6 +1,7 @@
 package com.samuel.spaceships.api.Application.Create;
 
-import com.samuel.spaceships.api.Application.Command;
+import com.samuel.spaceships.api.Domain.Bus.Command.Command;
 
-public record CreateSpaceshipCommand(String name, String franchise, double maxSpeed) implements Command {
+public record CreateSpaceshipCommand(String name, String franchise, double maxSpeed) implements
+    Command {
 }
--- src/main/java/com/samuel/spaceships/api/Application/Create/SpaceshipCreator.java
+++ src/main/java/com/samuel/spaceships/api/Application/Create/SpaceshipCreator.java
@@ -1,27 +1,44 @@
 package com.samuel.spaceships.api.Application.Create;
 
 import com.samuel.spaceships.api.Domain.EventBus;
-import com.samuel.spaceships.api.Domain.IdGenerator;
 import com.samuel.spaceships.api.Domain.Spaceship.Spaceship;
 import com.samuel.spaceships.api.Domain.Spaceship.Repository.SpaceshipRepository;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipFranchise;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipId;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipName;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipSpeed;
+import com.samuel.spaceships.api.Domain.UuidGenerator;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SpaceshipCreator {
 
-  private final IdGenerator idGenerator;
+  private final UuidGenerator uuidGenerator;
   private final SpaceshipRepository repository;
   private final EventBus eventBus;
+  private static final Logger logger = LoggerFactory.getLogger(SpaceshipCreator.class);
 
 
-  public Spaceship create(String name, String franchise, double maxSpeed) {
-    Spaceship spaceship = Spaceship.create(idGenerator.generate(), name, franchise, maxSpeed);
+
+  public void create(SpaceshipName name, SpaceshipFranchise franchise, SpaceshipSpeed maxSpeed) {
+    SpaceshipId id = generateUniqueSpaceshipId();
+
+    Spaceship spaceship = Spaceship.create(id, name, franchise, maxSpeed);
+
+    logger.debug("Creating starship with uuid \"{}\"...", id.value());
     repository.save(spaceship);
-
     eventBus.publish(spaceship.pullDomainEvents());
+  }
 
-    return spaceship;
+  private SpaceshipId generateUniqueSpaceshipId() {
+    SpaceshipId id;
+    do {
+      id = new SpaceshipId(uuidGenerator.generate());
+    } while (repository.existsById(id));
+    return id;
   }
 }
