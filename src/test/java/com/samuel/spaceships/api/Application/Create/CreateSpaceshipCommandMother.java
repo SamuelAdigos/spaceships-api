e9eee771bd806b7552c58f769ba3dd9c552e781e
Samuel Vila <samuel.vila@hpe.com>
2024-05-24 12:18:20 +0200

    Update tests to use parameterized UUID and refactor object creation
    In these modifications, the tests were updated to use a parameterized UUID instead of a single hardcoded value. CreateSpaceshipCommandMother has been refactored to create SpaceshipName, SpaceshipFranchise, and SpaceshipSpeed objects. Changes also include replacing the IdGenerator with the UuidGenerator in the SpaceshipsModuleUnitTestCase.

--- src/test/java/com/samuel/spaceships/api/Application/Create/CreateSpaceshipCommandMother.java
+++ src/test/java/com/samuel/spaceships/api/Application/Create/CreateSpaceshipCommandMother.java
@@ -1,36 +1,18 @@
 package com.samuel.spaceships.api.Application.Create;
 
-public class CreateSpaceshipCommandMother {
-  public static CreateSpaceshipCommand withDefaults() {
-    return new CreateSpaceshipCommand("Enterprise", "Star Trek", 9.975);
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipFranchise;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipFranchiseMother;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipName;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipNameMother;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipSpeed;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipSpeedMother;
+
+public final class CreateSpaceshipCommandMother {
+  public static CreateSpaceshipCommand create(SpaceshipName name, SpaceshipFranchise franchise, SpaceshipSpeed maxSpeed) {
+    return new CreateSpaceshipCommand( name.value(),franchise.value(), maxSpeed.value());
   }
 
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-    private String name = "Enterprise";
-    private String franchise = "Star Trek";
-    private double maxSpeed = 9.975;
-
-    public Builder withName(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public Builder withFranchise(String franchise) {
-      this.franchise = franchise;
-      return this;
-    }
-
-    public Builder withMaxSpeed(double maxSpeed) {
-      this.maxSpeed = maxSpeed;
-      return this;
-    }
-
-    public CreateSpaceshipCommand build() {
-      return new CreateSpaceshipCommand(name, franchise, maxSpeed);
-    }
+  public static CreateSpaceshipCommand random() {
+    return create(SpaceshipNameMother.random(), SpaceshipFranchiseMother.random(), SpaceshipSpeedMother.random());
   }
 }
--- src/test/java/com/samuel/spaceships/api/Application/SpaceshipsModuleUnitTestCase.java
+++ src/test/java/com/samuel/spaceships/api/Application/SpaceshipsModuleUnitTestCase.java
@@ -1,11 +1,11 @@
 package com.samuel.spaceships.api.Application;
 
-import com.samuel.spaceships.api.Application.Create.SpaceshipCreatedEventPublisher;
 import com.samuel.spaceships.api.Domain.DomainEvent;
 import com.samuel.spaceships.api.Domain.EventBus;
-import com.samuel.spaceships.api.Domain.IdGenerator;
 import com.samuel.spaceships.api.Domain.Spaceship.Repository.SpaceshipRepository;
 import com.samuel.spaceships.api.Domain.Spaceship.Spaceship;
+import com.samuel.spaceships.api.Domain.Spaceship.SpaceshipId;
+import com.samuel.spaceships.api.Domain.UuidGenerator;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,19 +15,27 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public abstract class SpaceshipsModuleUnitTestCase {
-  protected IdGenerator idGenerator;
+  protected UuidGenerator uuidGenerator;
   protected SpaceshipRepository repository;
   protected EventBus eventBus;
 
   @BeforeEach
   protected void setUp() {
-    idGenerator = mock(IdGenerator.class);
+    uuidGenerator = mock(UuidGenerator.class);
     repository = mock(SpaceshipRepository.class);
     eventBus = mock(EventBus.class);
   }
 
   public void shouldHaveSaved(Spaceship spaceship) {
     verify(repository, atLeastOnce()).save(spaceship);
+  }
+
+  public void shouldHaveCheckedExistence(SpaceshipId spaceshipId) {
+    verify(repository, atLeastOnce()).existsById(spaceshipId);
+  }
+
+  public void shouldHaveDeleted(SpaceshipId spaceshipId) {
+    verify(repository, atLeastOnce()).deleteById(spaceshipId);
   }
 
   public void shouldHavePublished(List<DomainEvent> domainEvents) {
