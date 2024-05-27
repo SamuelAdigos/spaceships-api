7fef5b2033844350d70196f886db633e60de5002
Samuel Vila <samuel.vila@hpe.com>
2024-05-27 23:15:51 +0200

    Update format checking functionality in Spaceship API
    Updated the format checking functionality in the Spaceship API. It now checks for improperly formatted UUIDs during the criteria-based search process instead of only during individual spaceship retrievals. Logs a warning message if an invalid UUID format is detected.

--- src/main/java/com/samuel/spaceships/api/Infrastructure/Aspect/WrongUuidFormatLoggingAspect.java
+++ src/main/java/com/samuel/spaceships/api/Infrastructure/Aspect/WrongUuidFormatLoggingAspect.java
@@ -1,6 +1,8 @@
 package com.samuel.spaceships.api.Infrastructure.Aspect;
 
 
+import com.samuel.spaceships.api.Domain.Criteria.FilterOperator;
+import com.samuel.spaceships.api.Domain.Criteria.Filters;
 import java.util.UUID;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -14,10 +16,21 @@ public class WrongUuidFormatLoggingAspect {
 
   private static final Logger logger = LoggerFactory.getLogger(WrongUuidFormatLoggingAspect.class);
 
-  @Before("execution(* com.samuel.spaceships.api.Application.Get.SpaceshipGetter.getSpaceshipById(..)) && args(id)")
-  public void logWrongFormat(String id) {
-    if (!isValidUUID(id)) {
-      logger.warn("Attempted to get a spaceship with an invalid UUID format: {}", id);
+  @Before("execution(* com.samuel.spaceships.api.Application.Search.SpaceshipsByCriteriaSearcher.search(..)) && args(filters,..)")
+  public void checkFiltersForId(Filters filters) {
+    filters.filters().stream()
+        .filter(filter ->
+            filter.field().value().trim().equalsIgnoreCase("id") && filter.operator().value()
+                .trim().toLowerCase().equals(FilterOperator.EQUAL.value()))
+        .forEach(filter -> {
+          System.out.println("Filter value: " + filter.value().value());
+          logWrongFormat(filter.value().value());
+        });
+  }
+
+  private void logWrongFormat(String value) {
+    if (!isValidUUID(value)) {
+      logger.warn("The UUID {} is not in the correct format", value);
     }
   }
 
