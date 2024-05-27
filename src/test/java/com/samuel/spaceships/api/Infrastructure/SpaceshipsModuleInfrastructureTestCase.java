700522744ce260c11d36e296c90ba07262acacb7
Samuel Vila <samuel.vila@hpe.com>
2024-05-27 23:18:31 +0200

    Update test configurations and clean up infrastructure test case
    Additional configuration classes were added to the BaseControllerTest's ContextConfiguration annotation. It now also includes MongoDbTestContainerConfig and RabbitMqTestContainerConfig alongside the existing SpaceshipsApiApplication.class. Concurrently, unnecessary variables and spaces in the SpaceshipsModuleInfrastructureTestCase class were removed, making the code cleaner and more organized.

--- src/test/java/com/samuel/spaceships/api/Infrastructure/SpaceshipsModuleInfrastructureTestCase.java
+++ src/test/java/com/samuel/spaceships/api/Infrastructure/SpaceshipsModuleInfrastructureTestCase.java
@@ -2,29 +2,27 @@ package com.samuel.spaceships.api.Infrastructure;
 
 import com.samuel.spaceships.api.Infrastructure.Persistence.Mapper.SpaceshipMapper;
 import com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.MongoSpaceshipRepository;
-import com.samuel.spaceships.api.Infrastructure.Persistence.Mongo.BaseSpaceshipMongoRepository;
+import com.samuel.spaceships.api.MongoDbTestContainerConfig;
 import com.samuel.spaceships.api.SpaceshipsApiApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = SpaceshipsApiApplication.class)
+@ContextConfiguration(classes = {SpaceshipsApiApplication.class, MongoDbTestContainerConfig.class})
 @SpringBootTest
 public abstract class SpaceshipsModuleInfrastructureTestCase {
 
-  @Autowired
-  protected BaseSpaceshipMongoRepository spaceshipMongoRepository;
   @Autowired
   protected SpaceshipMapper spaceshipMapper;
   @Autowired
   protected MongoSpaceshipRepository mongoSpaceshipRepository;
 
-  private final int MAX_ATTEMPTS                   = 3;
+  private final int MAX_ATTEMPTS = 3;
   private final int MILLIS_TO_WAIT_BETWEEN_RETRIES = 300;
 
   protected void eventually(Runnable assertion) throws Exception {
-    int     attempts = 0;
-    boolean allOk    = false;
+    int attempts = 0;
+    boolean allOk = false;
 
     while (attempts < MAX_ATTEMPTS && !allOk) {
       try {
@@ -36,7 +34,8 @@ public abstract class SpaceshipsModuleInfrastructureTestCase {
 
         if (attempts > MAX_ATTEMPTS) {
           throw new Exception(
-              String.format("Could not assert after some retries. Last error: %s", error.getMessage())
+              String.format("Could not assert after some retries. Last error: %s",
+                  error.getMessage())
           );
         }
 
